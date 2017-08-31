0bfbf0bbd9708beeba43006f94bb938c2d921e57
tamami <tamami.oka@gmail.com>
2017-08-31 14:29:45 +0700

    complete.. sudah ada form login-nya

---
+++ src/main/java/lab/aikibo/aplikasihalo/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package lab.aikibo.aplikasihalo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by tamami on 31/08/17.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("tamami")
+                .password("rahasia")
+                .roles("USER");
+    }
+
+}
